5b7498beb870bf61b41c8c1fbb555023bc27458f
silmara esteffany matias freire <20211148060020@ifrn.local>
2023-01-19 14:18:52 -0300

    teste

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-function trazerRepositorio() {
- axios
-    .get(' http://localhost:5000/correntista')
-    .then((response) => {
-      const repos = response.data;
-     console.log(repos);
+// function trazerRepositorio() {
+//  axios
+//     .get(' http://localhost:5000/correntista')
+//     .then((response) => {
+//       const repos = response.data;
+//      console.log(repos);
 
-     var lista=document.querySelector('#repos');
-    console.log(lista);
-    var nome = document.querySelector('#nome')
-    nome.innerHTML = repos[0].CodigoCorrentista
+// var lista=document.querySelector('#repos');
+// console.log(lista);
+// var nome = document.querySelector('#nome')
+// nome.innerHTML = repos[0].CodigoCorrentista
 
-    })
-    .catch((error) => console.error(error));
-}
-trazerRepositorio() 
+//  })
+// .catch((error) => console.error(error));
+//  }
+// trazerRepositorio() 
+
+// function log(){
+//     var done = 0;
+//     var mail= document.getElementById('mail');
+//     var mail2 = mail.value.toLowerCase();
+//    console.log(mail2);
+//     if (mail2 == "" ) {
+//       window.location = "log.html";
+//       done = 1;
+//     }
+//     if (done == 0) {
+//       alert("Dados incorretos, tente novamente");
+  
+//     }
+
+const mail =document.getElementById('mail');
+
+ mail.onclick = function(){ 
+   axios({
+    method:'GET',
+    url: 'http://localhost:5000/corclearrentista'
+   }).then(res=>{
+     console.log(res.data);
+   })
+ }
